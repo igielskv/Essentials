86f98218c749fd5c800a300f94f50ba528545236
Vladislav Igielski <igielskv@icloud.com>
2020-06-27 21:16:05 +0200

    Initial Commit

---
+++ Essentials/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Essentials
+//
+//  Created by Manoli on 27/06/2020.
+//  Copyright © 2020 macForce. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
